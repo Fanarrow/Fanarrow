5624ced48d6a49b3d5ad181a43122fff50bd9763
TantoFaz <tantofaz@gmail.com>
2022-08-24 13:33:47 -0300

    First commit

---
+++ menus/menus.js
@@ -0,0 +1,532 @@
+const menu = (prefix, NomeDoBot, sender) => {
+  
+// NÃO APAGUE ESSE ${NickDono} nem 
+//${numerodn} nem ${NomeDoBot} nem ${prefix} só se quiser apagar completo, não coloque nada ${dentro assim} ISSO SÃO DEFINIÇÕES QUE ESTÁ PUXANDO DO settings.json, da pasta dono, só pode altera a base de tudo, menos as definições, só se quiser apagar a definição completa.  
+  
+return `​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​
+╔┅═──•———————————————————─𒀸
+╠⊹ 
+╠⊹   ╭━─•⬕•𝕭𝖗𝖚𝖈𝖊•⬔•─━─━𒀸
+╠⊹   ┝≼≽ Seja bem vindo(a) ao menu 
+╠⊹   ┝≼≽ Prefixo: ⟮•  ${prefix} •⟯
+╠⊹   ┝≼≽ Usuário: @${sender.split("@")[0]}
+╠⊹   ╰─━─━─━──━──━─━─━━𒀸
+╠⊹   ╭━─━INFO DE CMD─━─━𒀸
+╠⊹   ┝≼≽ ${prefix}infopalavrão
+╠⊹   ┝≼≽ ${prefix}infolistanegra
+╠⊹   ┝≼≽ ${prefix}infobancarac
+╠⊹   ┝≼≽ ${prefix}infovotação
+╠⊹   ┝≼≽ ${prefix}infocontador
+╠⊹   ┝≼≽ ${prefix}infosorteio
+╠⊹   ┝≼≽ ${prefix}infobot
+╠⊹   ┝≼≽ ${prefix}idiomas
+╠⊹   ┝≼≽ ${prefix}bug (questione)
+╠⊹   ┝≼≽ ${prefix}avalie (o quao bom)
+╠⊹   ┝≼≽ ${prefix}sugestao (dica)
+╠⊹   ╰─━─━─━──━──━─━─━𒀸
+║
+╠⊹   ╭─━━─━OUTROS MENUS━─𒀸
+╠⊹   ┝≼≽ ${prefix}menudono
+╠⊹   ┝≼≽ ${prefix}efeitosimg
+╠⊹   ┝≼≽ ${prefix}menuadm
+╠⊹   ┝≼≽ ${prefix}menupremium
+╠⊹   ┝≼≽ ${prefix}logos
+╠⊹   ┝≼≽ ${prefix}brincadeiras
+╠⊹   ┝≼≽ ${prefix}alteradores
+╠⊹   ╰─━─━─━──━──━─━─━𒀸
+║
+╠⊹   ╭─━━PESQUISA/BAIXAR━─━─𒀸
+╠⊹   ┝≼≽ ${prefix}play (pesquisa ou link)
+╠⊹   ┝≼≽ ${prefix}play2 (pesquisa ou link)
+╠⊹   ┝≼≽ ${prefix}play3
+╠⊹   ┝≼≽ ${prefix}playmp4
+╠⊹   ┝≼≽ ${prefix}ytsearch
+╠⊹   ┝≼≽ ${prefix}ytmp4
+╠⊹   ┝≼≽ ${prefix}ytmp3
+╠⊹   ┝≼≽ ${prefix}tiktok +link
+╠⊹   ┝≼≽ ${prefix}tikto2 +link
+╠⊹   ┝≼≽ ${prefix}Tiktok (LINK) 
+╠⊹   ┝≼≽ ${prefix}Instagram (LINK) 
+╠⊹   ┝≼≽ ${prefix}Facebook (LINK) 
+╠⊹   ┝≼≽ ${prefix}Twitter (LINK) 
+╠⊹   ┝≼≽ ${prefix}celular (nome do modelo)
+╠⊹   ┝≼≽ ${prefix}mediafire (url)
+╠⊹   ┝≼≽ ${prefix}imgpralink
+╠⊹   ┝≼≽ ${prefix}videopralink
+╠⊹   ╰─━─━─━──━──━─━─━─━─𒀸
+║
+╠⊹   ╭─━─━─INFORMAÇÕES━─━─𒀸
+╠⊹   ┝≼≽ ${prefix}ping (velo)
+╠⊹   ┝≼≽ ${prefix}
+╠⊹   ┝≼≽ ${prefix}atividade
+╠⊹   ┝≼≽ ${prefix}rankativo
+╠⊹   ┝≼≽ ${prefix}checkativo @marca
+╠⊹   ┝≼≽ ${prefix}tabela (tabela-letras)
+╠⊹   ┝≼≽ ${prefix}ranklevel (de todos)
+╠⊹   ╰─━─━─━──━──━─━─━━𒀸
+║
+╠⊹   ╭──━─━─Jogos━─━─━─━𒀸
+╠⊹   ┝≼≽ ${prefix}Ppt (pedra-papel-tesoura) 
+╠⊹   ┝≼≽ ${prefix}Jogodavelha @marca
+╠⊹   ┝≼≽ ${prefix}Ttt (jogo da velha) 
+╠⊹   ┝≼≽ ${prefix}Cassino
+╠⊹   ┝≼≽ ${prefix}Quizanime 1 / 0
+╠⊹   ┝≼≽ ${prefix}Quizanimais 1 / 0
+╠⊹   ┝≼≽ ${prefix}Anagrama 1 / 0
+╠⊹   ╰─━─━─━──━──━─━─━━𒀸
+║
+╠⊹   ╭──━─Figurinhas━─━─𒀸
+╠⊹   ┝≼≽ ${prefix}Sfundo
+╠⊹   ┝≼≽ ${prefix}Emojimix 😉+🙂
+╠⊹   ┝≼≽ ${prefix}Roubar (TEXT/TEXT)
+╠⊹   ┝≼≽ ${prefix}Rename (TEXT/TEXT)
+╠⊹   ┝≼≽ ${prefix}Fsticker
+╠⊹   ┝≼≽ ${prefix}Sticker
+╠⊹   ┝≼≽ ${prefix}Togif
+╠⊹   ┝≼≽ ${prefix}Toimg
+╠⊹   ╰─━─━─━──━──━─━─━━𒀸
+║
+╠⊹   ╭━─━ATTPS/CMD-DE-TXT━𒀸
+╠⊹   ┝≼≽ ${prefix}attp  (txt)
+╠⊹   ┝≼≽ ${prefix}attp2  (txt)
+╠⊹   ┝≼≽ ${prefix}attp3  (txt)
+╠⊹   ┝≼≽ ${prefix}attp4  (txt)
+╠⊹   ┝≼≽ ${prefix}attp5  (txt)
+╠⊹   ┝≼≽ ${prefix}ttp  (txt)
+╠⊹   ╰─━─━─━──━──━─━─━━𒀸
+║
+╠⊹   ╭─━─━─CMD-BÁSICOS━─━─𒀸
+╠⊹   ┝≼≽ ${prefix}Traduzir Hello
+╠⊹   ┝≼≽ ${prefix}Calcular (1 + 1)
+╠⊹   ┝≼≽ ${prefix}gtts (linguagem)
+╠⊹   ┝≼≽ ${prefix}tagme
+╠⊹   ┝≼≽ ${prefix}emoji
+╠⊹   ┝≼≽ ${prefix}tabela
+╠⊹   ┝≼≽ ${prefix}conselhobiblico
+╠⊹   ┝≼≽ ${prefix}simi (fale algo) 
+╠⊹   ┝≼≽ ${prefix}perfil
+╠⊹   ┝≼≽ ${prefix}tomp3 
+╠⊹   ┝≼≽ ${prefix}fazernick (nick)
+╠⊹   ┝≼≽ ${prefix}bot
+╠⊹   ┝≼≽ ${prefix}wa.me 
+╠⊹   ┝≼≽ ${prefix}wa.me2 (marcar amigo e texto) 
+╠⊹   ╰─━─━─━──━──━─━─━━𒀸
+║
+╠⊹            •INFO/DONO•
+╠⊹   ╭─━─━─━─━─𒀸
+╠⊹   ┝≼≽ NOME : ${NickDono}
+╠⊹   ╰─━─━─━──━──━─━─𒀸
+╚┅═┅══════┅═══┅════┅
+
+`
+}
+
+exports.menu = menu
+
+// NÃO APAGUE ESSE ${NickDono} nem 
+//${numerodn} nem ${NomeDoBot} nem ${prefix} só se quiser apagar completo, não coloque nada ${dentro assim} ISSO SÃO DEFINIÇÕES QUE ESTÁ PUXANDO DO settings.json, da pasta dono, só pode altera a base de tudo, menos as definições, só se quiser apagar a definição completa.
+
+
+// MENU DE ADMINISTRADORES 
+
+const adms = (prefix, sender) => { 
+ 
+// NÃO APAGUE ESSE ${prefix}, não coloque nada ${dentro assim} ISSO SÃO DEFINIÇÕES QUE ESTÁ PUXANDO DO settings.json, da pasta dono, só pode altera a base de tudo, menos as definições, só se quiser apagar a definição completa. 
+
+	return `​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​
+
+╭⎆──❲ 𝐌𝐄𝐍𝐔 𝐃𝐄 𝑨𝑫𝑴𝑺 ❳──╮
+╎
+┝  ⎙ : @${sender.split("@")[0]}
+╎
+╰──────────╮
+╭──────────╯
+╎
+│⎘⃯⃗➢ ${prefix}ativacoes
+│⎘⃯⃗➢ ${prefix}autorepo 1 / 0
+│⎘⃯⃗➢ ${prefix}Kick [@] (pra-remover) 
+│⎘⃯⃗➢ ${prefix}Ban (responder-mensagem)
+│⎘⃯⃗➢ ${prefix}Promover [@] (Ser-ADM)
+│⎘⃯⃗➢ ${prefix}Rebaixar [@] (rebaixar-adm)
+│⎘⃯⃗➢ ${prefix}Totag (menciona-algo)
+│⎘⃯⃗➢ ${prefix}Grupo f/a
+│⎘⃯⃗➢ ${prefix}Status
+│⎘⃯⃗➢ ${prefix}Limpar (texto-invisível-gp)
+│⎘⃯⃗➢ ${prefix}Atividades (DO-GRUPO)
+│⎘⃯⃗➢ ${prefix}Linkgp
+│⎘⃯⃗➢ ${prefix}Grupoinfo
+│⎘⃯⃗➢ ${prefix}Hidetag (txt) (marcação)
+│⎘⃯⃗➢ ${prefix}Marcar (marca tds do gp)
+│⎘⃯⃗➢ ${prefix}Marcar2 (Marca-tds-Wa.me/)
+│⎘⃯⃗➢ ${prefix}Anagrama 1 / 0
+│⎘⃯⃗➢ ${prefix}Autofigu 1 / 0
+│⎘⃯⃗➢ ${prefix}Antidocumento 1 / 0  
+│⎘⃯⃗➢ ${prefix}Antipalavra 1 / 0
+│⎘⃯⃗➢ ${prefix}Antiloc 1 / 0  
+│⎘⃯⃗➢ ${prefix}Anticontato 1 / 0  
+│⎘⃯⃗➢ ${prefix}Antilink 1 / 0
+│⎘⃯⃗➢ ${prefix}Antilinkhard 1 / 0
+│⎘⃯⃗➢ ${prefix}AntiCatalogo 1 / 0
+│⎘⃯⃗➢ ${prefix}Antifake 1 / 0
+│⎘⃯⃗➢ ${prefix}Bemvindo 1 / 0
+│⎘⃯⃗➢ ${prefix}Antiimg 1 / 0
+│⎘⃯⃗➢ ${prefix}Antiaudio 1 / 0
+│⎘⃯⃗➢ ${prefix}Antivideo 1 / 0
+│⎘⃯⃗➢ ${prefix}Leveling 1 / 0  
+│⎘⃯⃗➢ ${prefix}Simih 1 / 0
+│⎘⃯⃗➢ ${prefix}Simih2 1 / 0
+│⎘⃯⃗➢ ${prefix}Descgp (TXT)
+│⎘⃯⃗➢ ${prefix}Nomegp (Nome)
+│⎘⃯⃗➢ ${prefix}Criartabela (ESCREVA-ALGO)
+│⎘⃯⃗➢ ${prefix}Tabelagp
+╎
+╰────────────────────────────────────╯
+
+`
+}
+
+exports.adms = adms
+
+// NÃO APAGUE ESSE ${prefix}, não coloque nada ${dentro assim} ISSO SÃO DEFINIÇÕES QUE ESTÁ PUXANDO DO settings.json, da pasta dono, só pode altera a base de tudo, menos as definições, só se quiser apagar a definição completa. 
+
+
+// MENU DE DONO
+
+const menudono = (prefix, sender) => {
+	
+// NÃO APAGUE ESSE ${prefix}, não coloque nada ${dentro assim} ISSO SÃO DEFINIÇÕES QUE ESTÁ PUXANDO DO settings.json, da pasta dono, só pode alterar ele tod0, menos as definições, só se quiser apagar a definição completa. 	
+
+return `​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​
+	
+╭⎆──❲ 𝐌𝐄𝐍𝐔 𝐃𝐎𝐍𝐎 ❳──╮
+│
+│ Usuário: @${sender.split("@")[0]}
+│
+│⎘⃯⃗➢ ${prefix}ativacoes_dono
+│⎘⃯⃗➢ ${prefix}fotomenu (url)
+│⎘⃯⃗➢ ${prefix}blockcmd  (cmd)
+│⎘⃯⃗➢ ${prefix}unblockcmd (cmd)
+│⎘⃯⃗➢ ${prefix}legendabv (oq qr)
+│⎘⃯⃗➢ ${prefix}legendasaiu (oq qr)
+│⎘⃯⃗➢ ${prefix}legendasaiu2 (oq qr)
+│⎘⃯⃗➢ ${prefix}legendabv2 (oq qr)
+│⎘⃯⃗➢ ${prefix}fundobemvindo (link-img)
+│⎘⃯⃗➢ ${prefix}fundosaiu (link-img)
+│⎘⃯⃗➢ ${prefix}serpremium
+│⎘⃯⃗➢ ${prefix}Prefixo_tipo2 simbolo
+│⎘⃯⃗➢ ${prefix}Prefixo_tipo_off
+│⎘⃯⃗➢ ${prefix}listabt
+│⎘⃯⃗➢ ${prefix}listagp
+│⎘⃯⃗➢ ${prefix}limparmsg (chat-do-bot)
+│⎘⃯⃗➢ ${prefix}antipalavrão 1 / 0
+│⎘⃯⃗➢ ${prefix}antiligar 1 / 0
+│⎘⃯⃗➢ ${prefix}antipv 1 / 0 (esse da block)
+│⎘⃯⃗➢ ${prefix}addpalavra (palavrão)
+│⎘⃯⃗➢ ${prefix}delpalavra (palavrão)
+│⎘⃯⃗➢ ${prefix}antipvon
+│⎘⃯⃗➢ ${prefix}antipvoff
+│⎘⃯⃗➢ ${prefix}ativo
+│⎘⃯⃗➢ ${prefix}ausente (fale-oq-faz)
+│⎘⃯⃗➢ ${prefix}delpremium @(marca)
+│⎘⃯⃗➢ ${prefix}addpremium @(marca)
+│⎘⃯⃗➢ ${prefix}clonar [@] (rouba ft de prf)
+│⎘⃯⃗➢ ${prefix}fotobot (img, = foto do BT)
+│⎘⃯⃗➢ ${prefix}descriçãogp (digite-algo)
+│⎘⃯⃗➢ ${prefix}limpar (limpa tds conversas)
+│⎘⃯⃗➢ ${prefix}block [@] (bloq de usar cmds) 
+│⎘⃯⃗➢ ${prefix}unblock [@] (desbloquear) 
+│⎘⃯⃗➢ ${prefix}setprefix  (prefixo-novo)
+│⎘⃯⃗➢ ${prefix}bangp
+│⎘⃯⃗➢ ${prefix}unbangp
+│⎘⃯⃗➢ ${prefix}dono2 @marca
+│⎘⃯⃗➢ ${prefix}dono3 @marca
+│⎘⃯⃗➢ ${prefix}dono4 @marca
+│⎘⃯⃗➢ ${prefix}dono5 @marca
+│⎘⃯⃗➢ ${prefix}bcgp (TM pra tds do gp)
+│⎘⃯⃗➢ ${prefix}transmitir -all / -pvs / -gps
+│⎘⃯⃗➢ ${prefix}transmitir2 (marcar)
+│
+╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈╯
+`
+
+}
+
+exports.menudono = menudono
+
+// MENU DE LOGOS 
+
+const menulogos = (prefix, sender) => {
+  
+// NÃO APAGUE ESSE ${prefix}, não coloque nada ${dentro assim} ISSO SÃO DEFINIÇÕES QUE ESTÁ PUXANDO DO settings.json, da pasta dono, só pode altera a base de tudo, menos as definições, só se quiser apagar a definição completa.  
+  
+  return `​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​
+
+╭⎆──━──⊱⛦⊰──━──╮
+│
+│    > LOGOS <
+│
+├⎆──━──⊱⛦⊰──━──╮
+│ ✰•LOGOS TOPS•✰
+├⎆──━──⊱⛦⊰──━──╯
+│ Usuário: @${sender.split("@")[0]}
+╰──────────┐
+╭──────────┴─┐
+│ Logos De 1 Texto
+├────────────
+│⎘⃯⃗➢ ${prefix}Txtquadrinhos (txt) 
+│⎘⃯⃗➢ ${prefix}HackNeon (txt) 
+│⎘⃯⃗➢ ${prefix}EquipeMascote (txt) 
+│⎘⃯⃗➢ ${prefix}FFavatar (txt) 
+│⎘⃯⃗➢ ${prefix}Gizquadro (txt) 
+│⎘⃯⃗➢ ${prefix}Angelglx (txt) 
+│⎘⃯⃗➢ ${prefix}WingEffect (txt) 
+│⎘⃯⃗➢ ${prefix}Angelwing (txt) 
+│⎘⃯⃗➢ ${prefix}Blackpink (txt) 
+│⎘⃯⃗➢ ${prefix}Girlmascote (txt) 
+│⎘⃯⃗➢ ${prefix}Mascotegame (txt) 
+│⎘⃯⃗➢ ${prefix}Fpsmascote (txt) 
+│⎘⃯⃗➢ ${prefix}Logogame (txt) 
+│⎘⃯⃗➢ ${prefix}Glitch2 (txt) 
+│⎘⃯⃗➢ ${prefix}3DGold (txt)
+│⎘⃯⃗➢ ${prefix}Placaloli (txt)
+│⎘⃯⃗➢ ${prefix}Phadow (txt)
+│⎘⃯⃗➢ ${prefix}Efeitoneon (txt)
+│⎘⃯⃗➢ ${prefix}Cemiterio (txt)
+│⎘⃯⃗➢ ${prefix}Metalgold (txt)
+│⎘⃯⃗➢ ${prefix}Narutologo (txt)
+│⎘⃯⃗➢ ${prefix}Fire (txt)
+│⎘⃯⃗➢ ${prefix}Romantic (txt)
+│⎘⃯⃗➢ ${prefix}Smoke (txt)
+│⎘⃯⃗➢ ${prefix}Papel (txt)
+│⎘⃯⃗➢ ${prefix}Lovemsg (txt)
+│⎘⃯⃗➢ ${prefix}Lovemsg2 (txt)
+│⎘⃯⃗➢ ${prefix}Lovemsg3 (txt)
+│⎘⃯⃗➢ ${prefix}Coffecup (txt)
+│⎘⃯⃗➢ ${prefix}Coffecup2 (txt)
+│⎘⃯⃗➢ ${prefix}Cup (txt)
+│⎘⃯⃗➢ ${prefix}Florwooden (txt)
+│⎘⃯⃗➢ ${prefix}Lobometal (txt)
+│⎘⃯⃗➢ ${prefix}Harryp (txt)
+│⎘⃯⃗➢ ${prefix}Txtborboleta (txt)
+│⎘⃯⃗➢ ${prefix}Madeira (txt)
+│⎘⃯⃗➢ ${prefix}Pornhub (txt)
+│⎘⃯⃗➢ ${prefix}Escudo (txt)
+│⎘⃯⃗➢ ${prefix}Transformer (txt)
+│⎘⃯⃗➢ ${prefix}America (txt)
+│⎘⃯⃗➢ ${prefix}Demongreen (txt)
+│⎘⃯⃗➢ ${prefix}Wetglass (txt)    
+│⎘⃯⃗➢ ${prefix}Toxic (txt)     
+│⎘⃯⃗➢ ${prefix}Neon3 (txt)   
+│⎘⃯⃗➢ ${prefix}Neondevil (txt) 
+│⎘⃯⃗➢ ${prefix}Neongreen (txt)
+│⎘⃯⃗➢ ${prefix}Lava (txt)
+│⎘⃯⃗➢ ${prefix}Halloween (txt)
+│⎘⃯⃗➢ ${prefix}Neondevil (txt)
+│⎘⃯⃗➢ ${prefix}DemonFire (txt)
+│⎘⃯⃗➢ ${prefix}DemonGreen (txt)
+│⎘⃯⃗➢ ${prefix}Thunderv2 (txt)
+│⎘⃯⃗➢ ${prefix}Thunder (txt)
+│⎘⃯⃗➢ ${prefix}Colaq (txt)
+│⎘⃯⃗➢ ${prefix}Luxury (txt)
+│⎘⃯⃗➢ ${prefix}Berry (txt)
+│⎘⃯⃗➢ ${prefix}Transformer (txt)
+│⎘⃯⃗➢ ${prefix}Matrix (txt)
+│⎘⃯⃗➢ ${prefix}Horror (txt)
+│⎘⃯⃗➢ ${prefix}Nuvem (txt)
+│⎘⃯⃗➢ ${prefix}Neon (txt)
+│⎘⃯⃗➢ ${prefix}Neon1 (txt)
+│⎘⃯⃗➢ ${prefix}Neon2 (txt)
+│⎘⃯⃗➢ ${prefix}Neon3d (txt)
+│⎘⃯⃗➢ ${prefix}NeonGreen (txt)
+│⎘⃯⃗➢ ${prefix}Neon3 (txt)
+│⎘⃯⃗➢ ${prefix}Neve (txt)
+│⎘⃯⃗➢ ${prefix}Areia (txt)
+│⎘⃯⃗➢ ${prefix}Vidro (txt)
+│⎘⃯⃗➢ ${prefix}Style (txt)
+│⎘⃯⃗➢ ${prefix}Pink (txt)
+│⎘⃯⃗➢ ${prefix}Carbon (txt)
+│⎘⃯⃗➢ ${prefix}Tetalblue (txt)
+│⎘⃯⃗➢ ${prefix}Toxic (txt)
+│⎘⃯⃗➢ ${prefix}Jeans (txt)
+│⎘⃯⃗➢ ${prefix}Ossos (txt)
+│⎘⃯⃗➢ ${prefix}Asfalto (txt)
+│⎘⃯⃗➢ ${prefix}Natal (txt)
+│⎘⃯⃗➢ ${prefix}Joker (txt)
+│⎘⃯⃗➢ ${prefix}Blood (txt)
+│⎘⃯⃗➢ ${prefix}Break (txt)
+│⎘⃯⃗➢ ${prefix}Fiction (txt)
+│⎘⃯⃗➢ ${prefix}3dstone (txt)
+│⎘⃯⃗➢ ${prefix}Lapis (txt)
+│⎘⃯⃗➢ ${prefix}Gelo (txt)
+│⎘⃯⃗➢ ${prefix}Rainbow (txt)
+│⎘⃯⃗➢ ${prefix}Metalfire (txt)
+╰──────────┐
+╭──────────┴─┐
+│ Logos De 2 Texto
+├────────────
+│⎘⃯⃗➢ ${prefix}Comporn (txt/txt) 
+│⎘⃯⃗➢ ${prefix}Glitch (txt/txt)
+│⎘⃯⃗➢ ${prefix}Glitch3 (txt/txt)
+│⎘⃯⃗➢ ${prefix}Grafity (txt-txt)
+│⎘⃯⃗➢ ${prefix}Space (txt/txt)
+│⎘⃯⃗➢ ${prefix}Marvel (txt/txt)
+│⎘⃯⃗➢ ${prefix}GamePlay (txt/txt)
+│⎘⃯⃗➢ ${prefix}Stone (txt/txt)
+│⎘⃯⃗➢ ${prefix}Steel (txt/txt)
+│⎘⃯⃗➢ ${prefix}Ffbanner (txt/txt) 
+│⎘⃯⃗➢ ${prefix}Mascoteavatar (txt/txt) 
+╰────────────────────────────────╯
+`
+}
+
+exports.menulogos = menulogos
+
+// MENU DE ALTERAR ÁUDIOS E VÍDEOS
+
+const alteradores = (prefix, sender) => {
+
+// NÃO APAGUE ESSE ${prefix}, não coloque nada ${dentro assim} ISSO SÃO DEFINIÇÕES QUE ESTÁ PUXANDO DO settings.json, da pasta dono, só pode altera a base de tudo, menos as definições, só se quiser apagar a definição completa. 
+
+return`
+╭──❲ 𝑽𝒊𝒅𝒆𝒐𝒔 - 𝑨𝒖𝒅𝒊𝒐𝒔 ❳──╮
+│
+├───────────────
+│ Usuário: @${sender.split("@")[0]}
+╰──────────┐
+╭──────────┴─┐
+│ Alterar Videos
+├────────────
+│⎘⃯⃗➢ ${prefix}Videolento (marca)
+│⎘⃯⃗➢ ${prefix}Videorapido (marca)
+│⎘⃯⃗➢ ${prefix}Videocontrario (marca)
+╰──────────┐
+╭──────────┴─┐
+│ Alterar Audios
+├────────────
+│⎘⃯⃗➢ ${prefix}Audiolento (marca)
+│⎘⃯⃗➢ ${prefix}Audiorapido (marca)
+│⎘⃯⃗➢ ${prefix}Grave (marca)
+│⎘⃯⃗➢ ${prefix}Grave2 (marca)
+│⎘⃯⃗➢ ${prefix}fast (marca)
+│⎘⃯⃗➢ ${prefix}Esquilo (marca)
+│⎘⃯⃗➢ ${prefix}Estourar (marca)
+│⎘⃯⃗➢ ${prefix}Bass (marca)
+│⎘⃯⃗➢ ${prefix}Bass2 (marca)
+│⎘⃯⃗➢ ${prefix}Vozmenino (marca)
+╰────────────────────────────────────╯
+`
+}
+
+exports.alteradores = alteradores
+
+// MENU PREMIUM 
+
+const menuprem = (prefix, sender) => { 
+
+// NÃO APAGUE ESSE ${prefix}, não coloque nada ${dentro assim} ISSO SÃO DEFINIÇÕES QUE ESTÁ PUXANDO DO settings.json, da pasta dono, só pode altera a base de tudo, menos as definições, só se quiser apagar a definição completa. 
+
+return `
+╭───❲ 𝑴𝑬𝑵𝑼 𝑷𝑹𝑬𝑴𝑰𝑼𝑴 ❳──╮
+├───────────────
+│ Usuário: @${sender.split("@")[0]}
+╰──────────┐
+╭──────────┴─┐
+│⎘⃯⃗➢ ${prefix}Destrava
+│⎘⃯⃗➢ ${prefix}Destrava2
+│⎘⃯⃗➢ ${prefix}ddd (numero)
+│⎘⃯⃗➢ ${prefix}cep (numero)
+│⎘⃯⃗➢ ${prefix}gerarcpf
+│⎘⃯⃗➢ ${prefix}validarcpf (numero do cpf)
+│⎘⃯⃗➢ ${prefix}gerarcnpj
+│⎘⃯⃗➢ ${prefix}validarcnpj (numero do cnpj)
+│⎘⃯⃗➢ ${prefix}premiumlist
+│⎘⃯⃗➢ ${prefix}metadinha
+│⎘⃯⃗➢ ${prefix}lerfoto (marca)
+│⎘⃯⃗➢ ${prefix}encurtalink (link)
+╰─────────────────────────────────╯
+`
+}
+
+exports.menuprem = menuprem
+
+// MENU DE BRINCADEIRAS.. 
+
+const brincadeiras = (prefix, sender) => {
+
+// NÃO APAGUE ESSE ${prefix}, não coloque nada ${dentro assim} ISSO SÃO DEFINIÇÕES QUE ESTÁ PUXANDO DO settings.json, da pasta dono, só pode altera a base de tudo, menos as definições, só se quiser apagar a definição completa. 
+
+return `​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​
+
+╭───❲  𝐵𝑟𝑖𝑛𝑐𝑎𝑑𝑒𝑖𝑟𝑎𝑠 ❳──╮
+├───────────────
+│ Usuário: @${sender.split("@")[0]}
+╰──────────┐
+╭──────────┴─┐
+│⎘⃯⃗➢ ${prefix}Gay (marca (@))
+│⎘⃯⃗➢ ${prefix}Feio (marca (@))
+│⎘⃯⃗➢ ${prefix}Corno (marca (@))
+│⎘⃯⃗➢ ${prefix}Vesgo (marca (@))
+│⎘⃯⃗➢ ${prefix}Bebado (marca (@))
+│⎘⃯⃗➢ ${prefix}Gostoso (marca (@))
+│⎘⃯⃗➢ ${prefix}Gostosa (marca (@))
+│⎘⃯⃗➢ ${prefix}Beijo (marca (@))
+│⎘⃯⃗➢ ${prefix}Matar (marca (@))
+│⎘⃯⃗➢ ${prefix}Tapa (marca (@))
+│⎘⃯⃗➢ ${prefix}Chute (marca (@))
+│⎘⃯⃗➢ ${prefix}Dogolpe (marca (@))   
+│⎘⃯⃗➢ ${prefix}Nazista (marca (@))
+│⎘⃯⃗➢ ${prefix}Chance (fale algo) 
+│⎘⃯⃗➢ ${prefix}Casal   
+│⎘⃯⃗➢ ${prefix}Rankgay     
+│⎘⃯⃗➢ ${prefix}Rankgado
+│⎘⃯⃗➢ ${prefix}Rankcorno  
+│⎘⃯⃗➢ ${prefix}Rankgostoso
+│⎘⃯⃗➢ ${prefix}Rankgostosa
+│⎘⃯⃗➢ ${prefix}Ranknazista
+│⎘⃯⃗➢ ${prefix}Rankotakus
+│⎘⃯⃗➢ ${prefix}Rankpau
+╰────────────────────────────╯
+`
+}
+
+exports.brincadeiras = brincadeiras
+
+// MENU DE EFEITOS DE IMAGEM, MONTAGEM Tops Kkk
+
+const efeitos = (prefix, sender) => {
+
+// NÃO APAGUE ESSE ${prefix}, não coloque nada ${dentro assim} ISSO SÃO DEFINIÇÕES QUE ESTÁ PUXANDO DO settings.json, da pasta dono, só pode altera a base de tudo, menos as definições, só se quiser apagar a definição completa. 
+
+return `​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​
+╭─────❲  𝐄𝐟𝐞𝐢𝐭𝐨𝐬 (𝐈𝐌𝐆) ❳──╮
+├───────────────
+│ Usuário: @${sender.split("@")[0]}
+╰──────────┐
+╭──────────┴─┐
+│⎘⃯⃗➢ ${prefix}Triggered (marcar)-(img)
+│⎘⃯⃗➢ ${prefix}Legenda (marcar)-(img)
+│⎘⃯⃗➢ ${prefix}Deletef (marcar)-(img)
+│⎘⃯⃗➢ ${prefix}Petmak (marcar)-(img)
+│⎘⃯⃗➢ ${prefix}Zombie (marcar)-(img)
+│⎘⃯⃗➢ ${prefix}Procurado (marcar)-(img)
+│⎘⃯⃗➢ ${prefix}Hitler (marcar)-(img)
+│⎘⃯⃗➢ ${prefix}Preso (marcar)-(img)
+│⎘⃯⃗➢ ${prefix}Lixo (marcar)-(img)
+│⎘⃯⃗➢ ${prefix}Figuborrada (marcar-(img) 
+│⎘⃯⃗➢ ${prefix}Comunismo (marcar)-(img) 
+│⎘⃯⃗➢ ${prefix}Arma (marcar)-(img)
+│⎘⃯⃗➢ ${prefix}Morto (marcar)-(img) 
+│⎘⃯⃗➢ ${prefix}Lgbt (marcar)-(img) 
+│⎘⃯⃗➢ ${prefix}Merda (marcar)-(img) 
+│⎘⃯⃗➢ ${prefix}Drip (marcar)-(img) 
+│⎘⃯⃗➢ ${prefix}Gta-v (marcar)-(img)
+│⎘⃯⃗➢ ${prefix}CuboM (marcar)-(img)
+│⎘⃯⃗➢ ${prefix}Coração (marcar)-(img)
+│⎘⃯⃗➢ ${prefix}Cadernoft (marcar)-(img)
+│⎘⃯⃗➢ ${prefix}PapelCortado (marcar)
+│
+╰────────────────────────────────────╯
+`
+}
+
+exports.efeitos = efeitos
