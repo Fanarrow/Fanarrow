1dc41e984512e6d3f1a13815b555e9feadb3b074
blabla <blablablabla@gmail.com>
2022-08-29 18:43:05 -0300

    First commit

---
+++ armor/js/criador.js
@@ -0,0 +1,27 @@
+const criador = (NomeDoBot) => {
+
+// NÃO APAGUE ESSE ${NickDono} nem 
+//${numerodn} nem ${NomeDoBot} nem ${prefix} só se quiser apagar completo, não coloque nada ${dentro assim} ISSO SÃO DEFINIÇÕES QUE ESTÁ PUXANDO DO settings.json, da pasta dono, só pode altera a base de tudo, menos as definições, só se quiser apagar a definição completa.
+
+return`╭━━━━━◉                                       ◉━━━━━╮
+       ╔┉✼┉════════┉✼┉╗    
+       ║  *𝐂𝐑𝐈𝐀𝐃𝐎𝐑*                 
+       ╚┉✼┉════════┉✼┉╝    
+╰━━━━━◉                                       ◉━━━━━╯
+
+
+𝐜𝐚𝐬𝐨 𝐪𝐮𝐞𝐢𝐫𝐚 𝐚𝐥𝐮𝐠𝐚𝐫 𝐨 𝐛𝐨𝐭 𝐨𝐮 𝐫𝐞𝐩𝐨𝐫𝐭𝐚𝐫 𝐚𝐥𝐠𝐮𝐦 𝐛𝐮𝐠 𝐨𝐮 𝐝𝐚 𝐬𝐮𝐚 𝐢𝐝𝐞𝐢𝐚/𝐚𝐯𝐚𝐥𝐢𝐚𝐜̧𝐚̃𝐨 𝐞́ 𝐬𝐨́ 𝐜𝐡𝐚𝐦𝐚𝐫
+╭═════════════◉
+║    ╔┉✼┉══════════════┉✼┉╗
+║    ║
+╠⊹   ┝≼≽ *Bot: 「𝕭𝖗𝖚𝖈𝖊」*
+╠⊹   ┝≼≽ *Criador: 𝕭𝖗𝖚𝖈𝖊*
+╠⊹   ┝≼≽ *Wa me Do Criador:* wa.me/+5521974836373
+╠⊹   ┝≼≽ *Twitter: Fanarrow*
+║    ║
+║    ╚┉✼┉══════════════┉✼┉╝
+╰═════════════◉
+`
+}
+
+exports.criador = criador
